1cf3ef81384dce51f420c1ddecdb67c04a9a61c0
kento <kento@toriwatari.bird>
2023-09-03 18:37:36 +0900

    ⚡ Update Header Component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
+import { useLocation } from "react-router-dom"
+
 import styles from "../styles/header.module.css"
 
 const Header = () => {
+  const path = useLocation()
+
+
   return (
     <header className={styles.header}>
       <h1 className={styles.headerTitle}>My Go App</h1>
+      <p className={styles.notice}>Version 0.1.0</p>
+
+      {path.pathname !== "/" && (
+        <p>ログアウト</p>
+      )}
     </header>
   )
 }
